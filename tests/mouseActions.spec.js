2a0581354fc01af32ea924e289967159d7e4d9b8
AnkitSrv <qa.ankit91@gmail.com>
2025-02-09 16:25:43 +0530

    moust hover new function added

--- tests/mouseActions.spec.js
+++ tests/mouseActions.spec.js
@@ -2,7 +2,7 @@ const {test,expect} = require ('@playwright/test')
 
 //////////////////////////////////////////////////////////  Mouse Hover ////////////////////////////////////////////////////////
 
-test ("Mouse Hover Demo    ", async ({page})=> {
+test.skip ("Mouse Hover Demo    ", async ({page})=> {
 
     await page.goto("https://www.thetestingworld.com/")
 
@@ -19,3 +19,20 @@ test ("Mouse Hover Demo    ", async ({page})=> {
 })
 
 
+//////////////////////////////////////////////////////////  Double click  ////////////////////////////////////////////////////////
+
+test ("Double click Demo",   async ({page})=> {
+
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    const doubleClickBtn1 =  await page.locator("//button[normalize-space()='Copy Text']")
+
+       await      doubleClickBtn1.dblclick()
+
+        const doubleClickBtn2= await page.locator("//input[@id='field2']")  
+        
+        await expect(doubleClickBtn2).toHaveValue('Hello World!')
+    
+
+
+})
